f83924ace8d6f9491a7a8cca82b631185dfb1370
devsantanu <sahoosanu599@gmail.com>
2026-05-29 16:15:27 +0530

    Add student grade system project

---
+++ student-grade-system-c.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+
+int main()
+{
+  char student_name[30];
+  
+  int mark[5],total=0,i,fail=0,highest_mark,lowest_mark;
+  
+  float average,percentage;
+  
+  printf("IMPORTANT NOTICE:\n");
+  
+  printf ("Do not use spaces in the student name.\n ");
+  
+  printf("===========================================\n");
+  
+  printf("Enter Student Name: ");
+  
+  scanf("%s",student_name);
+  
+  printf("\n");
+  
+  printf("Enter marks of 5 subjects:\n");
+  
+  for(i=0;i<5;i++)
+  {
+    printf("subject %d: ",i+1);
+       scanf("%d",&mark[i]);
+       
+       if(mark[i]<0||mark[i]>100)
+       {
+         printf("Invalid Marks Entered \n");
+         return 0;
+       }
+       
+       if(i==0)
+       {
+          highest_mark = mark[i];
+         lowest_mark = mark[i];
+       }
+
+          if(mark[i]>highest_mark)
+         {
+      highest_mark = mark[i];
+        }
+        
+        if(mark[i]<lowest_mark)
+         {
+      lowest_mark = mark[i];
+        }
+       
+       if(mark[i]<33)
+       {
+         printf("Subject %d :Fail\n",i+1);
+         
+         fail=1;
+         
+       }
+       else
+       {
+         printf("Subject %d :Pass\n",i+1); 
+       }
+       
+        total = total + mark[i];
+      
+         printf("\n");
+  }
+    
+  average = total/5.0;
+  
+  percentage = (total/500.0)*100;
+  
+  printf("Total = %d\n",total);
+  
+  printf("Average = %.2f\n",average);
+  
+  printf("Percentage = %.2f\n",percentage);
+  
+   printf("Highest_mark = %d\n",highest_mark);
+  
+  printf("Lowest_mark = %d\n",lowest_mark);
+  
+  if(fail==1)
+  {
+    printf("Grade = F\n");
+  }
+   else if(percentage >=90)
+  {
+     printf("Grade = A\n");
+  }
+  else if(percentage>=75&&percentage<90)
+  {
+    printf("Grade = B\n");
+  }
+  else if(percentage>=60)
+  {
+    printf("Grade = C\n");
+  
+  }
+  else if(percentage>=40)
+  {
+    printf("Grade = D\n");  
+  
+  }
+  else
+  {
+    printf("Grade = F\n");
+  }
+    if(fail==1)
+   {
+      printf("Overall Result = Fail\n");
+   }
+   else
+   {
+      printf("Overall Result = Pass\n");
+   }
+  return 0;
+}
